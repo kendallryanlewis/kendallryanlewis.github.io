f2b6d92527d613c08a262bb71249e0c491ed4625
Kendall lewis <kendall.ryan.lewis@gmail.com>
2024-04-18 22:01:16 -0500

    Updating layout

--- Cabinet-cocktails/js/script.js
+++ Cabinet-cocktails/js/script.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
     });
 
     // Register the GSAP plugin
-    gsap.registerPlugin(ScrollTrigger);
+   /* gsap.registerPlugin(ScrollTrigger);
 
     // Query all H1 elements and convert NodeList to Array
     const textTitles = Array.from(document.querySelectorAll(".fade-item"));
@@ -20,7 +20,7 @@ $(document).ready(function() {
         scrollTrigger: {
             trigger: ".phone", // The element that triggers the start and end of the animation
             start: "center center", // Start the animation when the center of ".phone" hits the center of the viewport
-            end: "center 100%", // End the animation when the center of ".phone" hits 60% from the top of the viewport
+            end: "top center", // End the animation when the center of ".phone" hits 60% from the top of the viewport
             endTrigger: ".text__last", // The element that defines the endpoint of the scrollTrigger
             pin: true, // Pin the ".phone" element when the scrollTrigger starts
             pinSpacing: true // Add spacing for the pinned element
@@ -42,5 +42,5 @@ $(document).ready(function() {
                 }
             }
         });
-    });
+    });*/
 });
